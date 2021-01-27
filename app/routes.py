6763f8858f5b322d0cc947366edff11b00cc4f9e
arvitaly <arvitaly@list.ru>
2021-01-28 01:07:25 +0800

    Part II: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Arvitaly'}
+    posts = [
+        {
+            'author': {'username': 'Mikhail'},
+            'body': 'Beautiful day in California!'
+        },
+        {
+            'author': {'username': 'Sergey'},
+            'body': 'The Shawshank Redemption movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
